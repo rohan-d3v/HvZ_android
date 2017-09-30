98a50f8bc8c8ad46c9c368165d01ad9dd181658f
Aaron Cutright <ahc49@case.edu>
2017-09-29 20:36:50 -0400

    setup project
    tested deployment via emulator, phone -> works
    fairly empty at this point, really just a demo

---
+++ app/src/main/java/edu/acase/hvz/hvz_app/HumanActivity.java
@@ -0,0 +1,13 @@
+package edu.acase.hvz.hvz_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HumanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_human);
+    }
+}
