8dfcfe6ed0c49cbd7ad0e97d05915a72ff4dc041
Aaron Cutright <ahc49@case.edu>
2017-10-13 21:13:02 -0400

    got a map working, added modals

---
+++ app/src/main/java/edu/acase/hvz/hvz_app/CommonDialogs.java
@@ -0,0 +1,42 @@
+package edu.acase.hvz.hvz_app;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.view.View;
+
+public class CommonDialogs {
+    public static void getHelpButtonDialog(Context context, View view) {
+        AlertDialog.Builder builder = getAlertBuilder(context);
+        builder.setTitle("Help")
+                .setIcon(android.R.drawable.ic_menu_help)
+                .setMessage("Usage info goes here")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss dialog
+                    }
+                })
+                .show();
+    }
+
+    public static void getInfoButtonDialog(Context context, View view) {
+        AlertDialog.Builder builder = getAlertBuilder(context);
+        builder.setTitle("Info")
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setMessage("Game info goes here")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss dialog
+                    }
+                })
+                .show();
+    }
+
+    public static AlertDialog.Builder getAlertBuilder(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            return new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+        else
+            return new AlertDialog.Builder(context);
+    }
+}
--- app/src/main/java/edu/acase/hvz/hvz_app/HumanActivity.java
+++ app/src/main/java/edu/acase/hvz/hvz_app/HumanActivity.java
@@ -9,59 +9,85 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HumanActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HumanActivity extends AppCompatActivity implements OnMapReadyCallback {
+   private GoogleMap gmap;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_human);
+
+        //MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapView);
+        //mapFragment.getMapAsync(this);
+
         final Button helpButton = (Button) findViewById(R.id.helpButton),
                      infoButton = (Button) findViewById(R.id.infoButton),
                      reportButton = (Button) findViewById(R.id.reportButton),
                      caughtButton = (Button) findViewById(R.id.caughtButton);
 
-        final Context context = this.getBaseContext();
-
+        final Context context = this;
         helpButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                AlertDialog.Builder builder;
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
-                } else {
-                    builder = new AlertDialog.Builder(context);
-                }
-                builder.setTitle("Delete entry")
-                        .setMessage("Are you sure you want to delete this entry?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // continue with delete
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        })
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+                CommonDialogs.getHelpButtonDialog(context, v);
             }
         });
         infoButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //onclick code
+                CommonDialogs.getInfoButtonDialog(context, v);
             }
         });
         reportButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //onclick code
+                AlertDialog.Builder builder = CommonDialogs.getAlertBuilder(context);
+                builder.setTitle("Sighted Zombie")
+                        .setIcon(android.R.drawable.presence_video_busy)
+                        .setMessage("Usage info goes here")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //dismiss dialog
+                            }
+                        })
+                        .show();
+
             }
         });
         caughtButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //onclick code
+                AlertDialog.Builder builder = CommonDialogs.getAlertBuilder(context);
+                builder.setTitle("Got Caught")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setMessage("Usage info goes here")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //dismiss dialog
+                            }
+                        })
+                        .show();
 
             }
         });
+    }
+
+    private AlertDialog.Builder getModalBuilder(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            return new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+        else
+            return new AlertDialog.Builder(context);
     }
 }
