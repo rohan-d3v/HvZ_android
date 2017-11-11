074217302dd8d31421ca8b8db7ba2c7a389a8cc8
rohanezio <rxr353@case.edu>
2017-11-10 19:32:54 -0500

    Week 7 commit: Forgot to commit the actual code sorry

---
+++ app/src/main/java/edu/acase/hvz/hvz_app/editActivity.java
@@ -0,0 +1,35 @@
+package edu.acase.hvz.hvz_app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class EditActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editactivity);
+
+        final LatLng latlng = (LatLng) getIntent().getParcelableExtra("location");
+
+        final EditText title = (EditText) findViewById(R.id.title);
+        Button boton = (Button) findViewById(R.id.save);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                MarkerOptions marker = new MarkerOptions().position(latlng);
+                if (title.getText() != null) {
+                    marker.title(title.getText().toString());
+                }
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("marker", marker);
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
