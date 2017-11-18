3e642f1d5b3a3c5ffecf7f0feb1f5f4b05207b74
rohanezio <rxr353@case.edu>
2017-11-17 21:16:39 -0500

    Commit 8: Bug fixes, Admin view

--- app/src/main/java/edu/acase/hvz/hvz_app/editActivity.java
+++ app/src/main/java/edu/acase/hvz/hvz_app/editActivity.java
@@ -1,17 +1,24 @@
 package edu.acase.hvz.hvz_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class EditActivity extends Activity {
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+ class editActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editactivity);
+        setContentView(R.layout.activity_edit);
 
         final LatLng latlng = (LatLng) getIntent().getParcelableExtra("location");
 
