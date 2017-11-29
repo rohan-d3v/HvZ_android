5354542bb9886a2c9cc58cae4e866ba4012e0d57
Aaron Cutright <ahc49@case.edu>
2017-11-29 18:25:45 -0500

    starting to set up report tracking

--- app/src/main/java/edu/acase/hvz/hvz_app/api/models/BaseReportModel.java
+++ app/src/main/java/edu/acase/hvz/hvz_app/api/models/BaseReportModel.java
@@ -2,15 +2,19 @@ package edu.acase.hvz.hvz_app.api.models;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 abstract class BaseReportModel {
     final int DATABASE_ID;
     LatLng location;
     Date timeSighted;
+    protected static List<BaseReportModel> allReports = new ArrayList<>();
 
     protected BaseReportModel(int DATABASE_ID) {
         this.DATABASE_ID = DATABASE_ID;
+        allReports.add(this);
     }
 
     abstract String snippet();
